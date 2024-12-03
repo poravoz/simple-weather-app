cbf03d4b75590a670e5602eabd188305e211b417
poravoz <116353344+poravoz@users.noreply.github.com>
2024-12-03 12:14:15 +0200

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { Logger } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,17 @@ async function bootstrap() {
   const port = configService.get('port');
   const apiPrefix = configService.get('apiPrefix');
   app.setGlobalPrefix(apiPrefix);
+
+  // Swagger
+  const config = new DocumentBuilder()
+  .setTitle("Weather TEST")
+  .setDescription("Swagger TEST")
+  .setVersion("1.0")
+  .build()
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(port);
   Logger.log(`Server running on http://localhost:${port}`, 'Bootstrap');
 }
